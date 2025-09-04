54529a9e46d181ecc08ffeba9fad79703aaa0930
5eungwon <kimseung0630@naver.com>
2025-09-05 03:05:55 +0900

    검색 화면 1차 구현

--- src/styles/Globalstyle.tsx
+++ src/styles/Globalstyle.tsx
@@ -6,13 +6,17 @@ html, body, #root {
   height:100%;
   margin: 0;
   padding: 0;
- &::-webkit-scrollbar {
-    width: 0px; /* 크롬, 사파리에서 스크롤바 숨기기 */
-    height: 0px;
-    display: none;
-  }
-
 }
-`
+   /* Firefox */
+  * {
+    scrollbar-width: none;          /* FF */
+    -ms-overflow-style: none;       /* IE/old Edge */
+  }
+  /* Chrome, Safari, Opera, Edge(Chromium) */
+  *::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+  }
+`;
 
 export default Globalstyle
